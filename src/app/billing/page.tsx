4a0bb298a6add43cea333ff651dc703c1e4c3a25
hasnatamir2 <hasnatamir2@gmail.com>
2025-09-11 10:01:12 +0400

    sidebar intead of nevigation menus for better experience

--- src/app/billing/page.tsx
+++ src/app/billing/page.tsx
@@ -5,22 +5,22 @@ import { SubscribeButton } from "@/components/stripe/subscribe-button";
 import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
 import React from "react";
 import { api } from "../../../convex/_generated/api";
-import { useAuth } from "@clerk/nextjs";
+import { SignInButton, useAuth } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
+import { Button } from "@/components/ui/button";
 
 const BillingPage = () => {
     const { userId } = useAuth();
     const dbUser = useQuery(api.queries.user.getUserByClerkId, {
-        clerkId: userId || ""
+        clerkId: userId || "",
     });
-    if (!userId) {
-        return null;
-    }
 
     return (
         <div className='md:my-16 my-4 px-4 text-center'>
-            <h2 className='md:text-4xl text-2xl font-bold'>Manage your Subscription</h2>
-            <p className="mt-4 text-sm">
+            <h2 className='md:text-4xl text-2xl font-bold'>
+                Manage your Subscription
+            </h2>
+            <p className='mt-4 text-sm'>
                 Billing details and subscription management will be handled
                 here.
             </p>
@@ -37,7 +37,11 @@ const BillingPage = () => {
                     <p>Unlimited snippets</p>
                 </CardContent>
                 <CardFooter>
-                    {dbUser?.subscriptionStatus !== "active" ? (
+                    {!userId ? (
+                        <SignInButton>
+                            <Button className='w-full'>Sign in</Button>
+                        </SignInButton>
+                    ) : dbUser?.subscriptionStatus !== "active" ? (
                         <SubscribeButton />
                     ) : (
                         <CancelButton />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,22 +7,16 @@ import {
     SignedIn,
     SignedOut,
     UserButton,
-    useAuth,
 } from "@clerk/nextjs";
 import Link from "next/link";
-import {
-    NavigationMenu,
-    NavigationMenuItem,
-    NavigationMenuList,
-    navigationMenuTriggerStyle,
-} from "./ui/navigation-menu";
+import { SidebarTrigger } from "./ui/sidebar";
 
 export function Header() {
-    const isAuth = useAuth();
-
     return (
-        <header className='flex h-16 items-center justify-between gap-4 border-b px-4'>
+        <header className='flex h-16 w-full items-center justify-between gap-4 border-b px-4'>
             <div className='flex gap-4 text-sm font-medium items-center'>
+                <SidebarTrigger />
+
                 <Link href='/' className='flex items-center gap-x-2 mr-2'>
                     <svg
                         fill='none'
@@ -35,37 +29,15 @@ export function Header() {
                             d='M38 0a6 6 0 0 1 6 6v32a6 6 0 0 1-6 6H6a6 6 0 0 1-6-6V6a6 6 0 0 1 6-6h32ZM22.982 9.105c-.208-1.081-1.756-1.081-1.964 0l-.85 4.421a1 1 0 0 1-1.666.541l-3.287-3.077c-.804-.752-2.056.158-1.589 1.155l1.911 4.077a1 1 0 0 1-1.03 1.417l-4.467-.558c-1.093-.136-1.571 1.336-.607 1.868l3.942 2.175a1 1 0 0 1 0 1.752l-3.942 2.175c-.964.532-.486 2.004.607 1.868l4.468-.558a1 1 0 0 1 1.03 1.417l-1.912 4.077c-.467.997.785 1.907 1.589 1.155l3.287-3.077a1 1 0 0 1 1.666.54l.85 4.422c.208 1.081 1.756 1.081 1.964 0l.85-4.421a1 1 0 0 1 1.666-.541l3.287 3.077c.804.752 2.056-.158 1.589-1.155l-1.911-4.077a1 1 0 0 1 1.03-1.417l4.467.558c1.093.136 1.572-1.336.607-1.868l-3.942-2.175a1 1 0 0 1 0-1.752l3.942-2.175c.965-.532.486-2.004-.607-1.868l-4.468.558a1 1 0 0 1-1.03-1.417l1.912-4.077c.467-.997-.785-1.907-1.589-1.155l-3.287 3.077a1 1 0 0 1-1.666-.54l-.85-4.422Z'
                         />
                     </svg>
-                    <span className='font-semibold hidden md:block'>Snippet Studio</span>
+                    <span className='font-semibold hidden md:block'>
+                        Snippet Studio
+                    </span>
                 </Link>
-                <NavigationMenu>
-                    <NavigationMenuList>
-                        <NavigationMenuItem asChild>
-                            {isAuth && (
-                                <Link
-                                    href='/dashboard'
-                                    className={navigationMenuTriggerStyle()}
-                                >
-                                    Dashboard
-                                </Link>
-                            )}
-                        </NavigationMenuItem>
-                        <NavigationMenuItem asChild>
-                            <Link
-                                href='/billing'
-                                className={navigationMenuTriggerStyle()}
-                            >
-                                Billing
-                            </Link>
-                        </NavigationMenuItem>
-                    </NavigationMenuList>
-                </NavigationMenu>
             </div>
             <div className='flex items-center gap-x-4'>
                 <SignedOut>
                     <SignInButton>
-                        <Button variant='ghost'>
-                            Sign in
-                        </Button>
+                        <Button variant='ghost'>Sign in</Button>
                     </SignInButton>
                     <SignUpButton>
                         <Button>Sign up</Button>
